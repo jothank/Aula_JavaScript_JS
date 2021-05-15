ef1e4312bdd4428a258388ab9f276743c7415774
jothank <jothank@hotmail.com>
2021-05-15 15:15:20 -0300

    aula

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,17 @@
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menos de idade");
+}
+
 /*var nome = "Jonathan Costa";
 var idade = 31;
 var idade2 = 18;
 //alert(nome + " tem " + idade);
 console.log(nome);
-console.log(idade + idade2);*/
-
+console.log(idade + idade2);
 var lista = ["maça","pera","laranja"];
 lista.push("uva");
 console.log(lista)
-console.log(lista.join(" - "))
+console.log(lista.join(" - "))*/
