ab40fe8134b1fdbd5fd57563be96c54686195558
jothank <jothank@hotmail.com>
2021-05-15 15:10:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# aula_JavaScript
+ aula_JavaScript
---
+++ javascript/javascript.js
@@ -0,0 +1,11 @@
+/*var nome = "Jonathan Costa";
+var idade = 31;
+var idade2 = 18;
+//alert(nome + " tem " + idade);
+console.log(nome);
+console.log(idade + idade2);*/
+
+var lista = ["maça","pera","laranja"];
+lista.push("uva");
+console.log(lista)
+console.log(lista.join(" - "))
