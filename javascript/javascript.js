dff112e8fa3bb99fa99e6bf69f79ba264da6000a
jothank <jothank@hotmail.com>
2021-05-18 23:25:27 -0300

    aula

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,29 @@
-var idade = prompt("Qual sua idade?");
+/*var d = new Date();
+
+alert(d.getDay());
+alert(d.getMonth());
+alert(d.getFullYear());*/
+
+/*var count;
+
+for(count=0; count <= 5; coun++){
+    alert(count);
+}*/
+
+/*var count =0;
+
+while(count < 5){
+    console.log(count);
+    alert(count);
+    count++;
+}*/
+
+/*var idade = prompt("Qual sua idade?");
 if (idade >= 18){
     alert("maior de idade");
 }else{
     alert("menos de idade");
-}
+}*/
 
 /*var nome = "Jonathan Costa";
 var idade = 31;
